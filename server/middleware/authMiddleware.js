8315e14b1a78f50cd08c214ae676a2f327eaaace
Kevin <devadderkevin@gmail.com>
2022-11-19 23:35:46 +0200

    feat(back-end): :sparkles: Add userRoutes

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/UserModel");
 
-const protect = asyncHandler(async (permission, req, res, next) => {
+const protect = asyncHandler(async (req, res, next) => {
   let token;
   if (
     req.headers.authorization &&
@@ -15,7 +15,7 @@ const protect = asyncHandler(async (permission, req, res, next) => {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       //get user from token
       req.user = await User.findById(decoded.id).select("-password");
-      if (req.user.permission !== permission) {
+      if (req.user.permission !== 'user' || req.user.permission !== 'admin') {
         res.status(401);
         throw new Error("Not authorized");
       } else {
@@ -32,4 +32,67 @@ const protect = asyncHandler(async (permission, req, res, next) => {
     throw new Error("Not authorized");
   }
 });
-module.exports = { protect };
+
+const admin = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      //get  token from header
+      token = req.headers.authorization.split(" ")[1];
+      //Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      //get user from token
+      req.user = await User.findById(decoded.id).select("-password");
+      if (req.user.permission !== 'admin') {
+        res.status(401);
+        throw new Error("Not authorized");
+      } else {
+        next();
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+});
+
+const special = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      //get  token from header
+      token = req.headers.authorization.split(" ")[1];
+      //Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      //get user from token
+      req.user = await User.findById(decoded.id).select("-password");
+      if (req.user.permission !== 'validator') {
+        res.status(401);
+        throw new Error("Not authorized");
+      } else {
+        next();
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+});
+
+module.exports = { protect, admin, special };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { RegisterUser, LogInUser } = require("../controllers/userController");
-router.post("/register", RegisterUser);
-router.post("/login", LogInUser);
+const {
+    RegisterUser,
+    LogInUser,
+    GetUsers,
+    RegisterSpecial,
+    EditUser,
+    EditOwnUser,
+    DeleteUser,
+} = require("../controllers/userController");
+const { validateLogin, validateRegister, validateEditUser } = require("../middleware/validators/userValidator");
+const { protect, admin } = require("../middleware/authMiddleware");
+
+router.post("/register", validateRegister, RegisterUser);
+router.post("/login", validateLogin, LogInUser);
+router.get("/", admin, GetUsers);
+router.post("/registerSpecial", admin, validateRegister, RegisterSpecial);
+router.put("/edit/:id", admin, validateEditUser, EditUser);
+router.put("/edit", protect, validateEditUser, EditOwnUser);
+router.delete("/delete/:id", admin, DeleteUser);
+
 module.exports = router;
