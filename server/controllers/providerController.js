131f1cdda1b2308e830f32342804fd9c08fcc65d
Kevin <devadderkevin@gmail.com>
2022-11-19 21:04:04 +0200

    feat(back-end): :sparkles: Add providerController

---
+++ server/controllers/providerController.js
@@ -0,0 +1,26 @@
+const asyncHandler = require('express-async-handler');
+const Provider = require('../models/ProviderModel');
+
+const AddProvider = asyncHandler(async (req, res) => {
+    const { name, type, city } = req.body;
+    
+    try {
+        // Check if provider already exists
+        const providerExists = await Provider.findOne({ name: name, type: type, city: city });
+
+        if (providerExists) {
+            res.status(400);
+            throw new Error('Provider already exists');
+        } else {
+            const provider = await Provider.create({
+                name: name,
+                type: type,
+                city: city,
+            });
+            res.status(201).json(provider);
+        }
+    } catch (error) {
+        res.status(500);
+        throw new Error("Error creating provider");
+    }
+});
