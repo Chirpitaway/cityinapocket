e891b9503aca82e26933800bb5a281ea63298b18
Kevin <devadderkevin@gmail.com>
2022-11-19 21:28:02 +0200

    feat(back-end): :sparkles: Add EditComment function

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -26,3 +26,39 @@ const AddComment = asyncHandler(async (req, res) => {
         throw new Error("Error adding comment");
     }
 });
+
+const EditComment = asyncHandler(async (req, res) => {
+    const { userId, userName, message, rating } = req.body;
+    
+    try {
+        const provider = await Provider.findById(req.params.id);
+        
+        if (provider) {
+            const comment = {
+                userId: userId,
+                userName: userName,
+                message: message,
+                rating: rating,
+            };
+            if (provider.comments.id(req.params.commentId)) {
+                if (provider.comments.id(req.params.commentId).userId == req.user._id) {
+                    provider.comments.id(req.params.commentId).set(comment);
+                    await provider.save();
+                    res.status(201).json(provider);
+                } else {
+                    res.status(401);
+                    throw new Error('Not authorized');
+                }
+            } else {
+                res.status(404);
+                throw new Error('Comment not found');
+            }
+        } else {
+            res.status(404);
+            throw new Error('Provider not found');
+        }
+    } catch (error) {
+        res.status(500);
+        throw new Error("Error editing comment");
+    }
+});
