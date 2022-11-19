9cf1bb73c4d023a52c83be1ca797542f911c1277
Kevin <devadderkevin@gmail.com>
2022-11-19 15:46:03 +0200

    feat(front-end): :sparkles: Add Header component
    \

--- client/cityinapocket/src/App.vue
+++ client/cityinapocket/src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
+    <app-header/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import AppHeader from '@/components/General/Header'
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
 
 <style lang="sass">
 
---
+++ client/cityinapocket/src/components/General/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+        <h1 class="logo">CityinaPocket</h1>
+        <div class="cta">
+            <div class="language-select">
+                <span>ENG</span>
+                <img src="@/assets/eng-flag.png" alt="English">
+            </div>
+        </div>
+    </header>
+</template>
