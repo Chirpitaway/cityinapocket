8ecf5447163ce7fd0fe13f0c3dde28220be617c1
Kevin <devadderkevin@gmail.com>
2022-11-19 21:30:09 +0200

    feat(back-end): :sparkles: Add DeleteComment function

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -62,3 +62,31 @@ const EditComment = asyncHandler(async (req, res) => {
         throw new Error("Error editing comment");
     }
 });
+
+const DeleteComment = asyncHandler(async (req, res) => {
+    try {
+        const provider = await Provider.findById(req.params.id);
+        
+        if (provider) {
+            if (provider.comments.id(req.params.commentId)) {
+                if (provider.comments.id(req.params.commentId).userId == req.user._id || req.user.permission == 'admin') {
+                    provider.comments.id(req.params.commentId).remove();
+                    await provider.save();
+                    res.status(201).json(provider);
+                } else {
+                    res.status(401);
+                    throw new Error('Not authorized');
+                }
+            } else {
+                res.status(404);
+                throw new Error('Comment not found');
+            }
+        } else {
+            res.status(404);
+            throw new Error('Provider not found');
+        }
+    } catch (error) {
+        res.status(500);
+        throw new Error("Error deleting comment");
+    }
+});
