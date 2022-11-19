f7da653d83896bdfc3f2a3ddc20b12cd1550bc22
Kevin <devadderkevin@gmail.com>
2022-11-19 16:07:34 +0200

    feat(back-end): :sparkles: Update ProviderModel

--- server/models/ProvidersModel.js
+++ server/models/ProvidersModel.js
@@ -5,20 +5,20 @@ const ProvidersSchema = mongoose.Schema({
     required: true,
     ref: "Providers",
   },
- type:{
-    type:String,
-    required:true,
- },
-
- ticketTypes: {
+  // Type of provider - e.g. restaurant, museum, transportation etc.
+  type: {
     type: String,
     required: true,
   },
+  ticketTypes: [{
+    type: String,
+    required: true,
+  }],
   city: {
     type: String,
     required: true,
   },
-  comments:[String],
+  comments: [String],
   users: [String],
 });
 module.exports = mongoose.model("Providers", ProvidersSchema);
