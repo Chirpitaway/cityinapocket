96989f35c2e9627bc976dac92e145aba5270fffe
Kevin <devadderkevin@gmail.com>
2022-11-19 21:36:00 +0200

    feat(back-end): :sparkles: Add EditTicketType function

--- server/controllers/ticketTypeController.js
+++ server/controllers/ticketTypeController.js
@@ -3,10 +3,10 @@ const Provider = require('../models/ProviderModel');
 
 const AddTicketType = asyncHandler(async (req, res) => {
     const { name, price, description } = req.body;
-    
+
     try {
         const provider = await Provider.findById(req.params.id);
-        
+
         if (provider) {
             const ticketType = {
                 name: name,
@@ -26,3 +26,33 @@ const AddTicketType = asyncHandler(async (req, res) => {
     }
 });
 
+const EditTicketType = asyncHandler(async (req, res) => {
+    const { name, price, description, duration } = req.body;
+
+    try {
+        const provider = await Provider.findById(req.params.id);
+
+        if (provider) {
+            const ticketType = {
+                name: name,
+                price: price,
+                description: description,
+                duration: duration,
+            };
+            if (provider.ticketTypes.id(req.params.ticketTypeId)) {
+                provider.ticketTypes.id(req.params.ticketTypeId).set(ticketType);
+                await provider.save();
+                res.status(201).json(provider);
+            } else {
+                res.status(404);
+                throw new Error('Ticket type not found');
+            }
+        } else {
+            res.status(404);
+            throw new Error('Provider not found');
+        }
+    } catch (error) {
+        res.status(500);
+        throw new Error("Error editing ticket type");
+    }
+});
