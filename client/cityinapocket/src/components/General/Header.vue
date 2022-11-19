4a5b6f063bae74749631623dfe4d55f146051c90
Kevin <devadderkevin@gmail.com>
2022-11-19 15:49:17 +0200

    feat(front-end): :sparkles: Add Header component style

--- client/cityinapocket/src/components/General/Header.vue
+++ client/cityinapocket/src/components/General/Header.vue
@@ -9,3 +9,27 @@
         </div>
     </header>
 </template>
+
+<style lang="sass" scoped>
+header
+    display: flex
+    justify-content: space-between
+    align-items: center
+    padding: 0 5vw
+    height: 10vh
+
+    .language-select
+        display: flex
+        align-items: center
+        cursor: pointer
+
+        span
+            margin-right: 1vw
+            font-size: 1.5rem
+            font-weight: 600
+            color: #000
+
+        img
+            width: 2rem
+            height: 2rem
+</style>
