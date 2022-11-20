e54543befae4a4d69ec0f5a9e615ab20c018659e
Kevin <devadderkevin@gmail.com>
2022-11-20 03:48:54 +0200

    feat(front-end): :sparkles: Add push to landing in home

--- client/cityinapocket/src/components/Home/index.vue
+++ client/cityinapocket/src/components/Home/index.vue
@@ -7,13 +7,15 @@
       :textColor="'#fff'"
       :backgroundColor="'#197D2F'"
       :isPrimary="true"
+      @clicked="$router.push('/landing')"
     />
-    <h2>or a</h2>
+    <h2>or</h2>
     <custom-button
       :buttonText="'Tourist'"
       :textColor="'#fff'"
       :backgroundColor="'#197D2F'"
       :isPrimary="true"
+      @clicked="$router.push('/landing')"
     />
   </div>
 </template>
@@ -30,5 +32,27 @@ export default {
   components: {
     customButton,
   },
+  mounted() {
+    if (!this.user) {
+      this.$router.push("/");
+    }
+  },
 };
 </script>
+
+<style lang="sass" scoped>
+.container
+  display: flex
+  flex-direction: column
+  align-items: center
+  justify-content: center
+  min-height: 90vh
+  h1
+    font-size: 2rem
+    margin-bottom: 1rem
+    color: var(--main-primary-color)
+  h2
+    font-size: 1.5rem
+    margin-top: 1rem
+    color: var(--main-primary-color)
+</style>
