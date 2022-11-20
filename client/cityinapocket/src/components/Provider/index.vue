3515fd03bec63291279cac8defad5573599e8719
Kevin <devadderkevin@gmail.com>
2022-11-20 11:58:19 +0200

    feat(front-end): :sparkles: Add comment form

--- client/cityinapocket/src/components/Provider/index.vue
+++ client/cityinapocket/src/components/Provider/index.vue
@@ -14,16 +14,21 @@
       <h3>{{ comment.userName }}</h3>
       <h5>{{ comment.message }}</h5>
     </div>
+
+    <textarea placeholder="Scrie un comentariu..."/>
+    <custom-button :buttonText="'Trimite!'" />
   </div>
 </template>
   
   <script>
 import provider from "@/components/General/Provider";
 import ProviderService from "../../services/ProviderService";
+import customButton from "@/components/General/Button";
 import { mapGetters } from "vuex";
 export default {
   components: {
     provider,
+    customButton,
   },
   data() {
     return {
@@ -51,11 +56,20 @@ export default {
   justify-content: center
   height: 100%
   text-align: center
-  padding: 0 5vw
+  padding: 0 5vw 5vh 5vw
   box-sizing: border-box
   color: var(--main-primary-color)
   h1
     font-size: 2rem
     margin-bottom: 10vh
-
+  textarea
+    width: 100%
+    height: 30vh
+    margin: 5vh 0
+    border: none
+    border-radius: 10px
+    padding: 1rem
+    box-sizing: border-box
+    font-size: 1rem
+    resize: none
 </style>
